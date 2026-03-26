cfd68410038e6235cb528dbe636de752fcb36d5b
yeonoh2402596 <jooyeonoh3526777@gmail.com>
2026-03-26 21:48:17 +0900

    4차 수정~

--- everytable/src/main/java/everytable/util/filter/AuthorityFilter.java
+++ everytable/src/main/java/everytable/util/filter/AuthorityFilter.java
@@ -11,7 +11,7 @@ import jakarta.servlet.FilterConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
-// import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.HttpFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
